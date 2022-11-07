588a676455e8100122f83de55f3ffc7e89e22df4
Hoang <hoang.le@cgi.com>
2022-11-06 18:29:36 -0800

    Added temp approved status filter to Facility Search

--- CCOF.Infrastructure.WebAPI/Models/DataverseSearch.cs
+++ CCOF.Infrastructure.WebAPI/Models/DataverseSearch.cs
@@ -4,7 +4,7 @@
     {
         public string? search { get; set; }
         public bool usefuzzy { get; } = true;
-        public string? filter { get; } = "account:ccof_accounttype eq 100000001,ccof_facilitystatus eq 100000001"; // Restrict Search to Facility account type with approved status only
+        public string? filter { get; } = "account:ccof_accounttype eq 100000001,(ccof_facilitystatus eq 100000001 or ccof_facilitystatus eq 100000002)"; // Restrict Search to Facility account type with approved/temp approved status only
         public bool returntotalrecordcount { get; } = true;
         public List<string> entities { get; } = new List<string> { "account" }; // Restrict Search to account table only
     }
