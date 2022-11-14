00b5d7d361396fc8aedba4e578f2b8e4995e238f
Richard <rma@sourceamerica.org>
2022-11-14 14:25:17 -0800

    update UserProfileController.cs
    update Fetchxml

--- CCOF.Infrastructure.WebAPI/Controllers/UserProfileController.cs
+++ CCOF.Infrastructure.WebAPI/Controllers/UserProfileController.cs
@@ -27,65 +27,135 @@ namespace CCOF.Infrastructure.WebAPI.Controllers
         {
             if (string.IsNullOrEmpty(userId)) return BadRequest("Invalid Request");
 
+//            var fetchXml = $@"<?xml version=""1.0"" encoding=""utf-16""?>
+//<fetch distinct=""true"" no-lock=""true"">
+//  <entity name=""ccof_bceid_organization"">
+//    <attribute name=""ccof_businessbceid"" />
+//    <attribute name=""ccof_organization"" />
+//    <link-entity name=""contact"" from=""contactid"" to=""ccof_businessbceid"" link-type=""inner"" alias=""BCeID"">
+//      <attribute name=""ccof_userid"" />
+//      <attribute name=""ccof_username"" />
+//      <filter type=""and"">
+//        <condition attribute=""ccof_userid"" operator=""eq"" value=""{userId}"" />
+//      </filter>
+//    </link-entity>
+//    <link-entity name=""account"" from=""accountid"" to=""ccof_organization"" link-type=""inner"" alias=""Organization"">
+//      <attribute name=""name"" />
+//      <attribute name=""accountnumber"" />
+//      <attribute name=""accountid"" />
+//      <attribute name=""ccof_accounttype"" />
+//      <link-entity name=""ccof_application"" from=""ccof_organization"" to=""accountid"" link-type=""outer"" alias=""Application"">
+//        <attribute name=""ccof_providertype"" />
+//        <attribute name=""ccof_name"" />
+//        <attribute name=""statuscode"" />
+//        <attribute name=""ccof_applicationid"" />
+//        <attribute name=""ccof_applicationtype"" />
+//        <attribute name=""ccof_organization"" />
+//        <attribute name=""ccof_programyear"" />
+//        <link-entity name=""ccof_program_year"" from=""ccof_program_yearid"" to=""ccof_programyear"" link-type=""outer"" alias=""ProgramYear"">
+//          <attribute name=""ccof_name"" />
+//          <attribute name=""statuscode"" />
+//          <attribute name=""ccof_program_yearid"" />
+//          <order attribute=""ccof_name"" descending=""true"" />
+//        </link-entity>
+//        <link-entity name=""ccof_application_basefunding"" from=""ccof_application"" to=""ccof_applicationid"" link-type=""outer"" alias=""CCOF"">
+//          <attribute name=""ccof_application_basefundingid"" />
+//          <attribute name=""ccof_name"" />
+//          <attribute name=""statuscode"" />
+//          <attribute name=""ccof_facility"" />
+//          <link-entity name=""account"" from=""accountid"" to=""ccof_facility"" link-type=""outer"" alias=""CCOF.Facility"">
+//            <attribute name=""name"" />
+//            <attribute name=""accountnumber"" />
+//          </link-entity>
+//        </link-entity>
+//        <link-entity name=""ccof_applicationccfri"" from=""ccof_application"" to=""ccof_applicationid"" link-type=""outer"" alias=""CCFRI"">
+//          <attribute name=""ccof_applicationccfriid"" />
+//          <attribute name=""ccof_name"" />
+//          <attribute name=""ccof_ccfrioptin"" />
+//          <attribute name=""ccof_facility"" />
+//          <attribute name=""statuscode"" />
+//          <link-entity name=""account"" from=""accountid"" to=""ccof_facility"" link-type=""outer"" alias=""CCFRI.Facility"">
+//            <attribute name=""name"" />
+//            <attribute name=""accountnumber"" />
+//          </link-entity>
+//        </link-entity>
+//        <link-entity name=""ccof_applicationecewe"" from=""ccof_application"" to=""ccof_applicationid"" link-type=""outer"" alias=""ECEWE"">
+//          <attribute name=""ccof_applicationeceweid"" />
+//          <attribute name=""ccof_name"" />
+//          <attribute name=""statuscode"" />
+//          <attribute name=""ccof_optintoecewe"" />
+//          <attribute name=""ccof_facility"" />
+//          <link-entity name=""account"" from=""accountid"" to=""ccof_facility"" link-type=""outer"" alias=""ECEWE.Facility"">
+//            <attribute name=""name"" />
+//            <attribute name=""accountnumber"" />
+//          </link-entity>
+//        </link-entity>
+//      </link-entity>
+//    </link-entity>
+//  </entity>
+//</fetch>";
+
+            var fetchData = new
+            {
+                ccof_userid = userId
+            };
             var fetchXml = $@"<?xml version=""1.0"" encoding=""utf-16""?>
-<fetch distinct=""true"" no-lock=""true"">
+<fetch>
   <entity name=""ccof_bceid_organization"">
     <attribute name=""ccof_businessbceid"" />
     <attribute name=""ccof_organization"" />
     <link-entity name=""contact"" from=""contactid"" to=""ccof_businessbceid"" link-type=""inner"" alias=""BCeID"">
       <attribute name=""ccof_userid"" />
       <attribute name=""ccof_username"" />
-      <filter type=""and"">
-        <condition attribute=""ccof_userid"" operator=""eq"" value=""{userId}"" />
+      <filter>
+        <condition attribute=""ccof_userid"" operator=""eq"" value=""{fetchData.ccof_userid/*F0269F4F9251408D9B7F1A63F90B153A*/}"" />
       </filter>
     </link-entity>
     <link-entity name=""account"" from=""accountid"" to=""ccof_organization"" link-type=""inner"" alias=""Organization"">
-      <attribute name=""name"" />
-      <attribute name=""accountnumber"" />
-      <attribute name=""accountid"" />
       <attribute name=""ccof_accounttype"" />
+      <attribute name=""name"" />
+      <attribute name=""accountid"" />
+      <attribute name=""accountnumber"" />
       <link-entity name=""ccof_application"" from=""ccof_organization"" to=""accountid"" link-type=""outer"" alias=""Application"">
-        <attribute name=""ccof_providertype"" />
-        <attribute name=""ccof_name"" />
-        <attribute name=""statuscode"" />
-        <attribute name=""ccof_applicationid"" />
-        <attribute name=""ccof_applicationtype"" />
         <attribute name=""ccof_organization"" />
+        <attribute name=""ccof_applicationtype"" />
+        <attribute name=""ccof_applicationid"" />
+        <attribute name=""ccof_name"" />
         <attribute name=""ccof_programyear"" />
+        <attribute name=""statuscode"" />
+        <attribute name=""ccof_providertype"" />
         <link-entity name=""ccof_program_year"" from=""ccof_program_yearid"" to=""ccof_programyear"" link-type=""outer"" alias=""ProgramYear"">
           <attribute name=""ccof_name"" />
-          <attribute name=""statuscode"" />
           <attribute name=""ccof_program_yearid"" />
+          <attribute name=""statuscode"" />
           <order attribute=""ccof_name"" descending=""true"" />
         </link-entity>
         <link-entity name=""ccof_application_basefunding"" from=""ccof_application"" to=""ccof_applicationid"" link-type=""outer"" alias=""CCOF"">
-          <attribute name=""ccof_application_basefundingid"" />
-          <attribute name=""ccof_name"" />
-          <attribute name=""statuscode"" />
           <attribute name=""ccof_facility"" />
+          <attribute name=""statuscode"" />
+          <attribute name=""ccof_application_basefundingid"" />
           <link-entity name=""account"" from=""accountid"" to=""ccof_facility"" link-type=""outer"" alias=""CCOF.Facility"">
             <attribute name=""name"" />
             <attribute name=""accountnumber"" />
           </link-entity>
         </link-entity>
         <link-entity name=""ccof_applicationccfri"" from=""ccof_application"" to=""ccof_applicationid"" link-type=""outer"" alias=""CCFRI"">
-          <attribute name=""ccof_applicationccfriid"" />
-          <attribute name=""ccof_name"" />
           <attribute name=""ccof_ccfrioptin"" />
+          <attribute name=""ccof_name"" />
           <attribute name=""ccof_facility"" />
           <attribute name=""statuscode"" />
+          <attribute name=""ccof_applicationccfriid"" />
           <link-entity name=""account"" from=""accountid"" to=""ccof_facility"" link-type=""outer"" alias=""CCFRI.Facility"">
-            <attribute name=""name"" />
             <attribute name=""accountnumber"" />
+            <attribute name=""accountid"" />
           </link-entity>
         </link-entity>
-        <link-entity name=""ccof_applicationecewe"" from=""ccof_application"" to=""ccof_applicationid"" link-type=""outer"" alias=""ECEWE"">
-          <attribute name=""ccof_applicationeceweid"" />
+        <link-entity name=""ccof_adjudication_ecewe"" from=""ccof_application"" to=""ccof_applicationid"" link-type=""outer"" alias=""ECEWE"">
           <attribute name=""ccof_name"" />
+          <attribute name=""ccof_organization"" />
+          <attribute name=""ccof_adjudication_eceweid"" />
           <attribute name=""statuscode"" />
-          <attribute name=""ccof_optintoecewe"" />
-          <attribute name=""ccof_facility"" />
-          <link-entity name=""account"" from=""accountid"" to=""ccof_facility"" link-type=""outer"" alias=""ECEWE.Facility"">
+          <link-entity name=""account"" from=""accountid"" to=""ccof_organization"" link-type=""outer"" alias=""ECEWE.Facility"">
             <attribute name=""name"" />
             <attribute name=""accountnumber"" />
           </link-entity>
@@ -108,13 +178,16 @@ namespace CCOF.Infrastructure.WebAPI.Controllers
                     var applicationId = records[0][0]["Application.ccof_applicationid"]; // Latest Application
                     if (applicationId != null)
                     {
-                        var values = records[0].Where(t => (string)t["Application.ccof_applicationid"] == applicationId.ToString());
-                        return Ok(values);
+                      var  values =  records[0].Where(t => (string)t["Application.ccof_applicationid"] == applicationId.ToString());
+                        //return Ok(values);
+                        return Ok(response.Content.ReadAsStringAsync().Result);
 
-                    }                                                                     // var values = records[0].Where(t => (int?)t["ProgramYear.statuscode"] == statusCode);
+
+                    }                                                                     
                     else
                     {
                         // var values = records[0].Where(t => (string)t["Application.ccof_applicationid"] == "");
+                        // var values = records[0].Where(t => (int?)t["ProgramYear.statuscode"] == statusCode);
                         return Ok(records[0]);
                     }
                 }
