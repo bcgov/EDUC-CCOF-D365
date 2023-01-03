3dd994dff5f060b3de76df9f32d383e24d4fc66d
Hoang <hoang.le@cgi.com>
2023-01-03 07:39:55 -0800

    Updated Facility Search with new Facility Statuses

--- CCOF.Infrastructure.WebAPI/Models/DataverseSearch.cs
+++ CCOF.Infrastructure.WebAPI/Models/DataverseSearch.cs
@@ -4,7 +4,7 @@
     {
         public string? search { get; set; }
         public bool usefuzzy { get; } = true;
-        public string? filter { get; } = "account:ccof_accounttype eq 100000001,(ccof_facilitystatus eq 100000001 or ccof_facilitystatus eq 100000002)"; // Restrict Search to Facility account type with approved/temp approved status only
+        public string? filter { get; } = "account:ccof_accounttype eq 100000001,ccof_facilitystatus ne 100000009"; // Restrict Search to Facility account type and excluding closed status
         public bool returntotalrecordcount { get; } = true;
         public List<string> entities { get; } = new List<string> { "account" }; // Restrict Search to account table only
     }
