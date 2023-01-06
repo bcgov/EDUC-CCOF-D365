cee568f979b24a731c59b2fabc6d8b29872f7acb
Hoang <hoang.le@cgi.com>
2023-01-06 09:28:32 -0800

    Added ccofstatus on Application payload

--- CCOF.Infrastructure.WebAPI/Models/CCOFModels.cs
+++ CCOF.Infrastructure.WebAPI/Models/CCOFModels.cs
@@ -5,11 +5,14 @@ namespace CCOF.Infrastructure.WebAPI.Models
     public class UserProfile
     {
         public string? ccof_username { get; set; }
+
         public string? ccof_userid { get; set; }
 
         [JsonPropertyName("Organization.accountid")]
+
         public string? organization_accountid { get; set; }
         [JsonPropertyName("Organization.accountnumber")]
+
         public string? organization_accountnumber { get; set; }
 
         [JsonPropertyName("Organization.name")]
@@ -22,6 +25,7 @@ namespace CCOF.Infrastructure.WebAPI.Models
         public bool? organization_ccof_formcomplete { get; set; }
 
         public Facility[]? facilities { get; set; }
+
         public Application? application { get; set; }
     }
 
@@ -31,7 +35,7 @@ namespace CCOF.Infrastructure.WebAPI.Models
         public string? accountnumber { get; set; }
         public string? name { get; set; }
         [JsonPropertyName("ccof_facilitystatus@OData.Community.Display.V1.FormattedValue")]
-        public string? ccof_facilitystatusODataCommunityDisplayV1FormattedValue { get; set; }
+        public string? ccof_facilitystatus_formatted { get; set; }
         public int? ccof_facilitystatus { get; set; }
         public string? ccof_facilitylicencenumber { get; set; }
         public bool? ccof_formcomplete { get; set; }
@@ -43,14 +47,14 @@ namespace CCOF.Infrastructure.WebAPI.Models
         public string? ccof_applicationid { get; set; }
         public string? ccof_name { get; set; }
         [JsonPropertyName("ccof_applicationtype@OData.Community.Display.V1.FormattedValue")]
-        public string? ccof_applicationtypeODataCommunityDisplayV1FormattedValue { get; set; }
+        public string? ccof_applicationtype_formatted { get; set; }
         public int? ccof_applicationtype { get; set; }
         public string? _ccof_programyear_value { get; set; }
         [JsonPropertyName("statuscode@OData.Community.Display.V1.FormattedValue")]
-        public string? statuscodeODataCommunityDisplayV1FormattedValue { get; set; }
+        public string? statuscode_formatted { get; set; }
         public int? statuscode { get; set; }
         [JsonPropertyName("ccof_providertype@OData.Community.Display.V1.FormattedValue")]
-        public string? ccof_providertypeODataCommunityDisplayV1FormattedValue { get; set; }
+        public string? ccof_providertype_formatted { get; set; }
         public int? ccof_providertype { get; set; }
         public string? _ccof_organization_value { get; set; }
         public int? ccof_unlock_ccof { get; set; }
@@ -60,6 +64,9 @@ namespace CCOF.Infrastructure.WebAPI.Models
         public int? ccof_unlock_declaration { get; set; }
         public bool? ccof_licensecomplete { get; set; }
         public bool? ccof_ecewe_eligibility_complete { get; set; }
+        [JsonPropertyName("ccof_ccofstatus@OData.Community.Display.V1.FormattedValue")]
+        public string? ccof_ccofstatus_formatted { get; set; }
+        public int? ccof_ccofstatus { get; set; }       
         public Ccof_Programyear? ccof_ProgramYear { get; set; }
         public Ccof_Application_Basefunding_Application[]? ccof_application_basefunding_Application { get; set; }
         public Ccof_Applicationccfri_Application_Ccof_Ap[]? ccof_applicationccfri_Application_ccof_ap { get; set; }
@@ -82,7 +89,7 @@ namespace CCOF.Infrastructure.WebAPI.Models
         public string? _ccof_application_value { get; set; }
         public string? _ccof_facility_value { get; set; }
         [JsonPropertyName("statuscode@OData.Community.Display.V1.FormattedValue")]
-        public string? statuscodeODataCommunityDisplayV1FormattedValue { get; set; }
+        public string? statuscode_formatted { get; set; }
         public int? statuscode { get; set; }
         public bool? ccof_formcomplete { get; set; }
     }
@@ -93,7 +100,7 @@ namespace CCOF.Infrastructure.WebAPI.Models
         public string? _ccof_application_value { get; set; }
         public string? _ccof_facility_value { get; set; }
         [JsonPropertyName("statuscode@OData.Community.Display.V1.FormattedValue")]
-        public string? statuscodeODataCommunityDisplayV1FormattedValue { get; set; }
+        public string? statuscode_formatted { get; set; }
         public int? statuscode { get; set; }
         public int? ccof_ccfrioptin { get; set; }
         public bool? ccof_formcomplete { get; set; }
@@ -108,7 +115,7 @@ namespace CCOF.Infrastructure.WebAPI.Models
         public string? _ccof_application_value { get; set; }
         public string? _ccof_facility_value { get; set; }
         [JsonPropertyName("statuscode@OData.Community.Display.V1.FormattedValue")]
-        public string? statuscodeODataCommunityDisplayV1FormattedValue { get; set; }
+        public string? statuscode_formatted { get; set; }
         public int? statuscode { get; set; }
         public int? ccof_optintoecewe { get; set; }
         public bool? ccof_formcomplete { get; set; }
