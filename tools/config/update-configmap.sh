30a6292031bba467089c0b73bc69192fe0f5c1b2
Trevor Richards <trev@trevdev.ca>
2024-10-17 15:00:31 -0700

    feat: add configmap generator for dev

---
+++ tools/config/update-configmap.sh
@@ -0,0 +1,34 @@
+set -euo pipefail
+
+readonly ENV_VAL=$1
+readonly APP_NAME=$2
+readonly OPENSHIFT_NAMESPACE=$3
+readonly DYNAMICS_AUTHENTICATION_SETTINGS=$4
+
+D365_CONFIGURATION=$(jq << JSON
+{
+  "Logging": {
+    "LogLevel": {
+      "Default": "Information",
+      "Microsoft.AspNetCore": "Warning"
+    }
+  },
+  "DynamicsAuthenticationSettings": $(cat "$DYNAMICS_AUTHENTICATION_SETTINGS")
+}
+JSON
+)
+readonly D365_CONFIGURATION
+echo "$D365_CONFIGURATION" > /tmp/appsettings.json
+
+echo
+echo Creating D365 config map "$APP_NAME-d365api-$ENV_VAL-config-map"
+oc create -n "$OPENSHIFT_NAMESPACE" configmap \
+  "$APP_NAME-d365api-$ENV_VAL-config-map" \
+  --from-file="appsettings.json=/tmp/appsettings.json" \
+  --dry-run -o yaml | oc apply -f -
+
+echo
+echo Setting environment variables for "$APP_NAME-d365api-$ENV_VAL" application
+oc -n "$OPENSHIFT_NAMESPACE" set env \
+  --from="configmap/$APP_NAME-d365api-$ENV_VAL-config-map" \
+  "dc/$APP_NAME-d365api-$ENV_VAL"
