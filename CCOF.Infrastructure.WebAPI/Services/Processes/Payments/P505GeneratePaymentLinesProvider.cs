d9c31de72c0db9286736a5578b5688f730e44ce2
Prashanthi Alemette Sivanarayana <p.alemettesivanarayana@cgi.com>
2025-06-23 16:27:02 -0400

    updates

--- CCOF.Infrastructure.WebAPI/Services/Processes/Payments/P505GeneratePaymentLinesProvider.cs
+++ CCOF.Infrastructure.WebAPI/Services/Processes/Payments/P505GeneratePaymentLinesProvider.cs
@@ -4,7 +4,7 @@ using CCOF.Infrastructure.WebAPI.Extensions;
 using CCOF.Infrastructure.WebAPI.Models;
 
 using CCOF.Infrastructure.WebAPI.Services.AppUsers;
-using CCOF.Infrastructure.WebAPI.Services.D365WebAPI;
+using CCOF.Infrastructure.WebAPI.Services.D365WebApi;
 using System.Net;
 using System.Text.Json.Nodes;
 using System.Text.Json;
@@ -14,47 +14,44 @@ using Microsoft.Extensions.Options;
 
 namespace CCOF.Infrastructure.WebAPI.Services.Processes.Payments
 {
-    public class P505GeneratePaymentLinesProvider(IOptionsSnapshot<ExternalServices> bccasApiSettings, ID365AppUserService appUserService, D365WebApi.ID365WebApiService d365WebApiService, ILoggerFactory loggerFactory, TimeProvider timeProvider) : ID365ProcessProvider
+    public class P505GeneratePaymentLinesProvider(IOptionsSnapshot<ExternalServices> bccasApiSettings, ID365AppUserService appUserService, ID365WebApiService d365WebApiService, ILoggerFactory loggerFactory, TimeProvider timeProvider) : ID365ProcessProvider
     {
         private readonly BCCASApi _BCCASApi = bccasApiSettings.Value.BCCASApi;
         private readonly ID365AppUserService _appUserService = appUserService;
         private readonly D365WebApi.ID365WebApiService _d365WebApiService = d365WebApiService;
         private readonly ILogger _logger = loggerFactory.CreateLogger(LogCategory.Process);
-      
+
         private readonly TimeProvider _timeProvider = timeProvider;
         private ProcessParameter? _processParams;
-        
+
 
         public Int16 ProcessId => Setup.Process.Payments.GeneratePaymentLinesId;
         public string ProcessName => Setup.Process.Payments.GeneratePaymentLinesName;
 
+        public Task<ProcessData> GetDataAsync()
+        {
+            throw new NotImplementedException();
+        }
+
         #region Data Queries
 
-       
-      
+
+
 
         #endregion
 
-  
 
-        public async Task<JsonObject> RunProcessAsync(ID365AppUserService appUserService, D365WebApi.ID365WebApiService d365WebApiService, ProcessParameter processParams)
+
+        public async Task<JsonObject> RunProcessAsync(ID365AppUserService appUserService, ID365WebApiService d365WebApiService, ProcessParameter processParams)
+
+        #region Validation & Setup
+
+
+
         {
-            #region Validation & Setup
-
-
-            //_logger.LogTrace(CustomLogEvent.Process, "Start processing payments for the funding {FundingId}.", processParams.Funding.FundingId);
-
-            //_processParams = processParams;
-
-            //Org? funding = await _fundingRepository!.GetFundingByIdAsync(new Guid(processParams.Org!.FundingId!), isCalculator: false);
-
-            //if (funding is null)
-            //{
-            //    _logger.LogError(CustomLogEvent.Process, "Unable to retrieve Funding record with Id {FundingId}", processParams.Funding!.FundingId);
-            //    return ProcessResult.Completed(ProcessId).SimpleProcessResult;
-            //}
-
-            
+            return ProcessResult.Completed(ProcessId).SimpleProcessResult;
+        }
+    
 
         
 
@@ -62,10 +59,8 @@ namespace CCOF.Infrastructure.WebAPI.Services.Processes.Payments
 
            
 
-           
 
-            return ProcessResult.Completed(ProcessId).SimpleProcessResult;
-        }
+       
 
     }
    
