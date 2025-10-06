14520fb05b6b355b070ce3ec3d9e1d9bb09a6526
Harpreet Hans <harpreet.hans@cgi.com>
2025-10-06 07:45:58 -0600

    Multiple FA templates javascript

---
+++ CCOF.Application.WebResources/Scripts/FormHandler/ccof_funding_agreement_template.js
@@ -0,0 +1,55 @@
+﻿"use strict";
+
+var CCOF = CCOF || {};
+CCOF.FATemplate = CCOF.FATemplate || {};
+CCOF.FATemplate.Ribbon = CCOF.FATemplate.Ribbon || {};
+
+//Formload logic starts here
+CCOF.FATemplate.Ribbon = {
+    //A function called on save
+    openCustomPageExisting: function (selectedItems, selectedControl) {
+        var selectedRecord = selectedItems[0];
+        var pageInput = {
+            pageType: "custom",
+            name: "ccof_managefatemplates_5d936",
+            recordId: selectedRecord.Id.replace("{", "").replace("}", ""),
+            entityName: selectedRecord.TypeName
+        };
+        var navigationOptions = {
+            target: 1
+        };
+        Xrm.Navigation.navigateTo(pageInput, navigationOptions)
+            .then(
+                function () {
+                    selectedControl.refresh();
+                }
+            ).catch(
+                function (error) {
+                    console.log(error);
+                    selectedControl.refresh();
+                }
+            );
+    },
+    openCustomPageNew: function (selectedEntityName, primaryEntityName, firstPrimaryItemId, primaryControl, selectedControl) {
+        var pageInput = {
+            pageType: "custom",
+            name: "ccof_managefatemplates_5d936",
+            recordId: firstPrimaryItemId.replace("{", "").replace("}", ""),
+            entityName: primaryEntityName
+        };
+        var navigationOptions = {
+            target: 1
+        };
+        Xrm.Navigation.navigateTo(pageInput, navigationOptions)
+            .then(
+                function () {
+                    selectedControl.refresh();
+                }
+            ).catch(
+                function (error) {
+                    console.log(error);
+                    selectedControl.refresh();
+                }
+            );
+    }
+}
