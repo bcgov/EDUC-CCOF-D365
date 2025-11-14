d5f36cbe93b9a30fe6b02851d9db681253581754
Anjana1010 <146029972+Anjana1010@users.noreply.github.com>
2025-11-14 13:56:37 -0800

    Updated plugin to include ece FAC for monthly report (#131)
    Co-authored-by: anjana.yadav <anjana.yadav@gov.bc.ca>

--- CCOF.Infrastructure.Plugins/ECEWE/GenerateMonthlyReportNumber.cs
+++ CCOF.Infrastructure.Plugins/ECEWE/GenerateMonthlyReportNumber.cs
@@ -2,9 +2,11 @@
 using Microsoft.Xrm.Sdk.Query;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace CCOF.Infrastructure.Plugins.ECEWE
 {
@@ -12,6 +14,7 @@ namespace CCOF.Infrastructure.Plugins.ECEWE
     {
         public void Execute(IServiceProvider serviceProvider)
         {
+           
             // Obtain the tracing service
             ITracingService tracingService =
             (ITracingService)serviceProvider.GetService(typeof(ITracingService));
@@ -31,6 +34,10 @@ namespace CCOF.Infrastructure.Plugins.ECEWE
                 IOrganizationServiceFactory serviceFactory =
                     (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
                 IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+                Entity facility = service.Retrieve("account", entity.GetAttributeValue<EntityReference>("ccof_facility").Id, new ColumnSet("accountnumber"));
+             
+
                 tracingService.Trace("Starting ECE WE Facility plugin");
 
                 try
@@ -47,8 +54,11 @@ namespace CCOF.Infrastructure.Plugins.ECEWE
                             "<filter type='and'>" +
                             "<condition attribute='ccof_base_report_id' operator='eq' uitype='ccof_ece_monthly_report'  value='{"+ base_Report.Id+ "}' />" +
                             "</filter></entity></fetch>";
-                        tracingService.Trace(fetchXml);
+
+
+                       
                         EntityCollection ece_report = service.RetrieveMultiple(new FetchExpression(fetchXml));
+                    
                         if (ece_report.Entities.Count > 0)
                         {
                             var version = Convert.ToInt32(ece_report.Entities[0].Attributes["ccof_version"]) + 1;
@@ -73,19 +83,54 @@ namespace CCOF.Infrastructure.Plugins.ECEWE
                     }
                     else
                     {
-                        tracingService.Trace("base report"+entity.GetAttributeValue<EntityReference>("ccof_facility").Id);
-                        Entity facility = service.Retrieve("account", entity.GetAttributeValue<EntityReference>("ccof_facility").Id, new ColumnSet("accountnumber"));
-                       var name = DateTime.UtcNow.Date.ToString("MMyyyy");
+                        tracingService.Trace("base report main"+entity.GetAttributeValue<EntityReference>("ccof_facility").Id);
+                          var name = DateTime.UtcNow.Date.ToString("MMyyyy");
+                        if(facility.Attributes.Contains("accountnumber"))
                         name = name + Convert.ToString(facility["accountnumber"]).Split('-').Last() + "-01";
                         
-                        entity["ccof_name"] = name.ToString();
+                         entity["ccof_name"] = name.ToString();
                         entity["ccof_version"] = 1;
-                       
+                     
 
 
                     }
-                   
+                    tracingService.Trace("Entering in fetch");
 
+                    var ecefac_fetch = $"<fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='false'>" +
+                    "<entity name='ccof_adjudication_ecewe_facility'>" +
+                    "<attribute name='ccof_adjudication_ecewe_facilityid'/>" +
+                    "<attribute name='ccof_optinstartmonth'/>" +
+                    "<attribute name='createdon'/>" +
+                    "<order attribute = 'createdon' descending = 'true'/>" +
+                    "<filter type='and'>" +
+                    "<condition attribute='ccof_facility' operator='eq'  uitype='account' value='"+facility.Id+"'/>" +
+                    "<condition attribute='statuscode' operator='in'>" +
+                    "<value>5</value>" +
+                    "<value>4</value>" +
+                    "</condition>" +
+                    "</filter>" +
+                    "<link-entity name='ccof_application' from='ccof_applicationid' to='ccof_areyouapublicsectoremployer' link-type='inner' alias='am'>" +
+                    "<link-entity name='ccof_program_year' from='ccof_program_yearid' to='ccof_programyear' link-type='inner' alias='program'>" +
+                    "<filter type='and'>" +
+                    "<condition attribute='ccof_intakeperiodstart' operator='on-or-before' value='"+ DateTime.UtcNow.Date.ToString("yyyy-MM-dd") + "'/>" +
+                    "<filter type='or'>" +
+                    "<condition attribute='ccof_intakeperiodend' operator='on-or-after' value='"+ DateTime.UtcNow.Date.ToString("yyyy-MM-dd") + "'/>" +
+                    "<condition attribute='ccof_intakeperiodend' operator='null'/>" +
+                    "</filter>" +
+                    "</filter>" +
+                    "</link-entity>" +
+                    "</link-entity>" +
+                    "</entity></fetch>";
+                    tracingService.Trace("base ECE Fac fetch" + ecefac_fetch);
+                    EntityCollection ecefac = service.RetrieveMultiple(new FetchExpression(ecefac_fetch));
+                    tracingService.Trace("base ECE Fac" + ecefac.Entities[0].Id);
+
+                    if (ecefac.Entities.Count > 0)
+                    {
+                        entity["ccof_ece_we_facility_id"] = new EntityReference("ccof_adjudication_ecewe_facility", ecefac.Entities[0].Id);
+
+
+                    }
                 }
 
                 catch (Exception ex)
