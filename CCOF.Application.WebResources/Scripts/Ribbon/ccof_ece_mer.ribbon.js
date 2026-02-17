bc82044a418389ed019903f77cb8be27eea7190b
Harpreet Hans <harpreet.hans@cgi.com>
2026-02-17 09:43:50 -0700

    Added ribbon script for monthly ece report

---
+++ CCOF.Application.WebResources/Scripts/Ribbon/ccof_ece_mer.ribbon.js
@@ -0,0 +1,36 @@
+﻿// A webresource for AccountRibbon only
+
+//Create Namespace Object if its defined
+var CCOF = CCOF || {};
+CCOF.ECE_MER = CCOF.ECE_MER || {};
+CCOF.ECE_MER.Ribbon = CCOF.ECE_MER.Ribbon || {};
+
+CCOF.ECE_MER.Ribbon = {
+    // description
+    RecalculateMER: function (primarycontrol, primaryEntiityId) {
+         
+        var gridContext = primarycontrol;
+        var pageInput = {
+            pageType: "custom",
+            name: "ccof_monthlyecereportrecalculatehours_7e8cc",
+            recordId: primaryEntiityId
+        };
+        var navigationOptions = {
+            target: 2,
+            position: 1,
+            height: 306,
+            width: 703,
+        };
+        Xrm.Navigation.navigateTo(pageInput, navigationOptions)
+            .then(
+                function () {
+                    primarycontrol?.refresh();
+                    console.log("Success");
+                }
+            ).catch(
+                function (error) {
+                    console.log(Error);
+                }
+            );
+    }
+}
